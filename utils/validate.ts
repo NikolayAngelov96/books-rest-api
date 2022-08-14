6e9d7fb99c259b69a5db108133da7f84179712cc
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-08-14 19:18:50 +0300

    add correct data type for book

--- utils/validate.ts
+++ utils/validate.ts
@@ -1,11 +1,20 @@
 import { Book } from "@prisma/client";
 
-export const validateBook = (book: Book) => {
+export type BookData = {
+  title: string;
+  year: number;
+  description?: string;
+  authorId: string;
+};
+
+export const validateBook = (book: BookData) => {
   const { title, year, description, authorId } = book;
 
   if (!title) {
     throw new Error("Title is required");
   } else if (!year) {
     throw new Error("Year is required");
+  } else if (!authorId) {
+    throw new Error("Please provide authorId");
   }
 };
