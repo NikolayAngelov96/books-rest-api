14ded135cdd2c608d984488b27ea9eb5556c8374
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-08-16 09:57:10 +0300

    add author data validation

--- utils/validate.ts
+++ utils/validate.ts
@@ -28,3 +28,33 @@ export const validateBook = (book: BookData) => {
     throw new Error("Description must be a string or null");
   }
 };
+
+export type AuthorData = {
+  firstName: string;
+  lastName: string;
+  nationality: string;
+};
+
+export const validateAuthor = (author: AuthorData) => {
+  const { firstName, lastName, nationality } = author;
+
+  if (!firstName) {
+    throw new Error("First name is required");
+  } else if (!lastName) {
+    throw new Error("Last name is required");
+  } else if (!nationality) {
+    throw new Error("Nationality is required");
+  }
+
+  if (firstName && typeof firstName !== "string") {
+    throw new Error("First name should be of type string");
+  }
+
+  if (lastName && typeof lastName !== "string") {
+    throw new Error("Last name should be of type string");
+  }
+
+  if (nationality && typeof nationality !== "string") {
+    throw new Error("Nationality should be of type string");
+  }
+};
