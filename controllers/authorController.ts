b7bfbedd9828a74aacc2fc14abcbcd3ed4317171
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-08-16 10:04:16 +0300

    add post /authors endpoint

--- controllers/authorController.ts
+++ controllers/authorController.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { prisma } from "../index";
+import { AuthorData, validateAuthor } from "../utils/validate";
+import { getError } from "../utils/getError";
 
 const router = express.Router();
 
@@ -7,11 +9,32 @@ router.get("/", async (req: Request, res: Response) => {
   const authors = await prisma.author.findMany({
     orderBy: {
       firstName: "asc",
-      lastName: "asc",
     },
   });
 
   res.status(200).json(authors);
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const authorData: AuthorData = req.body;
+
+    validateAuthor(authorData);
+
+    const author = await prisma.author.create({
+      data: {
+        firstName: authorData.firstName,
+        lastName: authorData.lastName,
+        nationality: authorData.nationality,
+      },
+    });
+
+    res.status(201).json(author);
+  } catch (error) {
+    const { statusCode, message } = getError(error);
+
+    res.status(statusCode).json({ message });
+  }
 });
 
 export default router;
